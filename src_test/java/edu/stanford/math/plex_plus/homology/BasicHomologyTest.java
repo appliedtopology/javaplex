0b80d52540e1f8dc76db9f6ad54ac54d7046731a
Andrew Tausz <andrew.tausz@gmail.com>
2010-03-29 21:42:16 +0000

    Small change - added comments to tests.

--- src_test/java/edu/stanford/math/plex_plus/homology/BasicHomologyTest.java
+++ src_test/java/edu/stanford/math/plex_plus/homology/BasicHomologyTest.java
@@ -5,19 +5,31 @@ import edu.stanford.math.plex_plus.homology.FullSimplicialComplex;
 import edu.stanford.math.plex_plus.homology.SimplicialComplex;
 
 
-
+/**
+ * Very basic tests of homology related data structures.
+ * 
+ * @author Andrew Tausz
+ *
+ */
 public class BasicHomologyTest {	
 	public static void main(String[] args) {
 		basicTest();
 	}
 	
 	public static void basicTest() {
+		// create a new simplicial complex
 		SimplicialComplex complex = new FullSimplicialComplex();
+		
+		// add simplices
 		complex.addSimplex(new ArraySimplex(new int[]{0, 1}));
 		complex.addSimplex(new ArraySimplex(new int[]{1, 2}));
 		complex.addSimplex(new ArraySimplex(new int[]{2, 0}));
 		complex.addSimplex(new ArraySimplex(new int[]{2, 3, 4, 5}));
+		
+		// print the entire complex
 		System.out.println(complex.toString());
+		
+		// print the 2-boundary matrix in sparse form 
 		System.out.println(complex.getBoundaryMatrix(1).toString());	
 	}
 }
--- src_test/java/edu/stanford/math/plex_plus/structures/GenericFreeModuleTest.java
+++ src_test/java/edu/stanford/math/plex_plus/structures/GenericFreeModuleTest.java
@@ -11,19 +11,29 @@ import edu.stanford.math.plex_plus.math.structures.IntFreeModule;
 import edu.stanford.math.plex_plus.math.structures.ModularIntField;
 import edu.stanford.math.plex_plus.math.structures.RationalField;
 
-
+/**
+ * 
+ * Basic test of free module arithmetic.
+ * 
+ * @author Andrew Tausz
+ *
+ */
 public class GenericFreeModuleTest {
 
 	public static void main(String[] args) {
+		
+		// Formal sums of String objects with Fraction coefficients
 		GenericFreeModule<Fraction, String> M = new GenericFreeModule<Fraction, String>(RationalField.getInstance());
 		GenericFormalSum<Fraction, String> sum = M.multiply(5, M.add(M.add("b", "b"), "a"));
 		System.out.println(sum.toString());
 		
+		// Formal sums of Simplex objects with Fraction coefficients
 		GenericFreeModule<Fraction, Simplex> C = new  GenericFreeModule<Fraction, Simplex>(RationalField.getInstance());
 		GenericFormalSum<Fraction, Simplex> chain = C.add(new ArraySimplex(new int[]{0, 1}), new ArraySimplex(new int[]{2, 1}));
 		chain = C.add(chain, chain);
 		System.out.println(chain);
 		
+		// Formal sums of Simplex objects with int coefficients
 		IntFreeModule<Simplex> D = new IntFreeModule<Simplex>(ModularIntField.getInstance(3));
 		IntFormalSum<Simplex> chain2 = D.add(new ArraySimplex(new int[]{0, 1}), new ArraySimplex(new int[]{2, 1}));
 		chain2 = D.add(chain2, chain2);
