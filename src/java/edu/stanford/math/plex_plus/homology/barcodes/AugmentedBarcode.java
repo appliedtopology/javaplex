8c1cf4e1c9db8fd627ca34ef42c6561366f71bc2
Andrew Tausz <andrew.tausz@gmail.com>
2010-06-24 23:35:13 +0000

    Added augmented barcode collection for storing generators.

---
+++ src/java/edu/stanford/math/plex_plus/homology/barcodes/AugmentedBarcode.java
@@ -0,0 +1,78 @@
+package edu.stanford.math.plex_plus.homology.barcodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stanford.math.plex_plus.datastructures.IntFormalSum;
+import edu.stanford.math.plex_plus.datastructures.pairs.GenericPair;
+import edu.stanford.math.plex_plus.utility.ExceptionUtility;
+
+public class AugmentedBarcode<T> {
+	private final int dimension;
+	private final List<GenericPair<PersistenceInterval, IntFormalSum<T>>> intervals = new ArrayList<GenericPair<PersistenceInterval, IntFormalSum<T>>>();
+	/**
+	 * This constructor initializes the barcode to be empty, with the
+	 * specified dimension.
+	 * 
+	 * @param dimension the dimension to initialize to
+	 */
+	public AugmentedBarcode(int dimension) {
+		this.dimension = dimension;
+	}
+	
+	/**
+	 * Gets the dimension of the barcode.
+	 * 
+	 * @return the dimension of the barcode
+	 */
+	public int getDimension() {
+		return this.dimension;
+	}
+	
+	/**
+	 * This function adds the specified interval to the barcode. The interval
+	 * is added to the end of the already existing list of intervals.
+	 * 
+	 * @param interval the PersistentInterval to add
+	 */
+	public void addInterval(PersistenceInterval interval, IntFormalSum<T> generatingCycle) {
+		ExceptionUtility.verifyNonNull(interval);
+		ExceptionUtility.verifyNonNull(generatingCycle);
+		this.intervals.add(new GenericPair<PersistenceInterval, IntFormalSum<T>>(interval, generatingCycle));
+	}
+	
+	/**
+	 * This function returns the number of "active" intervals at
+	 * a particular point.
+	 * 
+	 * @param point the point to query
+	 * @return the number of intervals containing the supplied point
+	 */
+	public int getSliceCardinality(double point) {
+		int cardinality = 0;
+		
+		for (GenericPair<PersistenceInterval, IntFormalSum<T>> pair: this.intervals) {
+			if (pair.getFirst().containsPoint(point)) {
+				cardinality++;
+			}
+		}
+		
+		return cardinality;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("Dimension: " + this.dimension + "\n");
+		for (GenericPair<PersistenceInterval, IntFormalSum<T>> pair: this.intervals) {
+			builder.append(pair.getFirst().toString());
+			builder.append(": ");
+			builder.append(pair.getSecond().toString());
+			builder.append("\n");
+		}
+		
+		return builder.toString();
+	}
+	
+}
---
+++ src/java/edu/stanford/math/plex_plus/homology/barcodes/AugmentedBarcodeCollection.java
@@ -0,0 +1,80 @@
+package edu.stanford.math.plex_plus.homology.barcodes;
+
+import edu.stanford.math.plex_plus.datastructures.IntFormalSum;
+import edu.stanford.math.plex_plus.utility.ExceptionUtility;
+import gnu.trove.iterator.TIntObjectIterator;
+import gnu.trove.map.hash.TIntIntHashMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
+
+public class AugmentedBarcodeCollection<T> {
+	private final TIntObjectHashMap<AugmentedBarcode<T>> barcodeMap = new TIntObjectHashMap<AugmentedBarcode<T>>();
+	
+	/**
+	 * This function adds an interval at the specified dimension.
+	 * 
+	 * @param dimension the dimension to add to
+	 * @param interval the interval to add
+	 */
+	public void addInterval(int dimension, PersistenceInterval interval, IntFormalSum<T> generatingCycle) {
+		ExceptionUtility.verifyNonNull(interval);
+		if (!this.barcodeMap.containsKey(dimension)) {
+			this.barcodeMap.put(dimension, new AugmentedBarcode<T>(dimension));
+		}
+		this.barcodeMap.get(dimension).addInterval(interval, generatingCycle);
+	}
+	
+	/**
+	 * This function adds the specified finite interval [start, end] at the
+	 * supplied dimension.
+	 * 
+	 * @param dimension the dimension to add to
+	 * @param start the starting point of the interval
+	 * @param end the ending point of the interval
+	 */
+	public void addInterval(int dimension, double start, double end, IntFormalSum<T> generatingCycle) {
+		this.addInterval(dimension, new PersistenceInterval(start, end), generatingCycle);
+	}
+	
+	/**
+	 * This function adds the specified semi-infinite interval [start, infinity)
+	 * at the supplied dimension.
+	 * 
+	 * @param dimension the dimension to add to
+	 * @param start the starting point of the interval
+	 */
+	public void addInterval(int dimension, double start, IntFormalSum<T> generatingCycle) {
+		this.addInterval(dimension, new PersistenceInterval(start), generatingCycle);
+	}
+	
+	/**
+	 * This function computes the Betti numbers for a particular filtration
+	 * value. It returns the results as a map which maps the dimension to the
+	 * Betti number.
+	 * 
+	 * @param filtrationValue the filtrationValue to compute the Betti numbers at
+	 * @return a TIntIntHashMap mapping dimension to the Betti number
+	 */
+	public TIntIntHashMap getBettiNumbers(double filtrationValue) {
+		TIntIntHashMap map = new TIntIntHashMap();
+		
+		for (TIntObjectIterator<AugmentedBarcode<T>> iterator = this.barcodeMap.iterator(); iterator.hasNext(); ) {
+			iterator.advance();
+			map.put(iterator.key(), iterator.value().getSliceCardinality(filtrationValue));
+		}
+		
+		return map;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		for (TIntObjectIterator<AugmentedBarcode<T>> iterator = this.barcodeMap.iterator(); iterator.hasNext(); ) {
+			iterator.advance();
+			builder.append(iterator.value().toString());
+			builder.append("\n");
+		}
+		
+		return builder.toString();
+	}
+}
