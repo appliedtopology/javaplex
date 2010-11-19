ac89898665b26712f86f8a9a378040bc5cc15be6
Andrew Tausz <andrew.tausz@gmail.com>
2010-11-19 20:21:48 +0000

    Small update.

--- src/matlab/hom_complex/test_aw_optimization.m
+++ src/matlab/hom_complex/test_aw_optimization.m
@@ -1,7 +1,7 @@
 
 clc; clear; close all;
 
-domain_size = 10;
+domain_size = 5;
 codomain_size = 4;
 
 % create the domain and codomain simplicial complexes
@@ -28,22 +28,31 @@ codomain_points = codomain_points + ones(codomain_size, 1) * [3, 0];
 domain_aw_matrix = convert_sparse_matrix(streams.utility.StreamUtility.createAlexanderWhitneyMatrix(domain_stream));
 codomain_aw_matrix = convert_sparse_matrix(streams.utility.StreamUtility.createAlexanderWhitneyMatrix(codomain_stream));
 
+[f, A, b, Aeq, beq, lb, ub] = create_max_lp(cycle_sum, homotopies);
+[x, fval, exitflag, output, lambda] = linprog(f, A, b, Aeq, beq, lb, ub);
+fval
+% find a random corner point of the set of optima
+[f, A, b, Aeq, beq, lb, ub] = create_max_lp(cycle_sum, homotopies, ceil(fval));
+
+K = size(homotopies, 1);
 
 loss_function = @(x) sum(x.^2);
 %loss_function = @(x) sum(abs(x));
 
-objective = @(c) alexander_whitney_loss_function(compute_mapping(cycle_sum, homotopies, c), domain_aw_matrix, codomain_aw_matrix, loss_function);
-c_0 = randn(size(homotopies, 1), 1);
+objective = @(c) alexander_whitney_loss_function(compute_mapping(cycle_sum, homotopies, c(1:K)), domain_aw_matrix, codomain_aw_matrix, loss_function);
+c_0 = randn(size(lb, 1), 1);
 
-[x, fval, exitflag, output] = fminunc(objective, c_0);
+%[x, fval, exitflag, output] = fminunc(objective, c_0);
+[x, fval, exitflag, output] = fmincon(objective, c_0, A, b, Aeq, beq, lb, ub);
+
 %{
 opts.tol = 1e-6;
 opts.maxIts = 500;
 opts.alg = 'N07';
 x = tfocs(objectivize(objective), [], [], c_0, opts)
 %}
-
-map = compute_mapping(cycle_sum, homotopies, x);
+%%
+map = compute_mapping(cycle_sum, homotopies, x(1:K));
 map = (abs(map) > 1e-2) .* map
 
 map = normalize_rows(map);
