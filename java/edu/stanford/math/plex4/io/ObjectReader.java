df894a6c0b848b55d91b3b0316122ffee263225b
Andrew Tausz <andrew.tausz@gmail.com>
2010-12-10 02:57:25 +0000

    Corrected mistake.

--- java/edu/stanford/math/plex4/io/ObjectReader.java
+++
@@ -1,7 +0,0 @@
-package edu.stanford.math.plex4.io;
-
-import java.io.IOException;
-
-public interface ObjectReader<T> {
-	T importFromFile(String path) throws IOException;
-}
--- java/edu/stanford/math/plex4/io/ObjectWriter.java
+++
@@ -1,8 +0,0 @@
-package edu.stanford.math.plex4.io;
-
-import java.io.IOException;
-
-public interface ObjectWriter<T> {
-	void writeToFile(T object, String path) throws IOException;
-	String getExtension();
-}
