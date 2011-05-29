5e64d6a18bb7670d70a4f2f3599649581f40c7c7
henryhughadams@gmail.com <henryhughadams@gmail.com@17b1e2b0-e439-11de-a347-f34a4f72eb7d>
2011-05-29 06:45:09 +0000

    More changes in section 5, started changing section 6. Still lots to fix in these sections.

--- src/matlab/for_distribution/tutorial_examples/image_patch_examples.m
+++ src/matlab/for_distribution/tutorial_examples/image_patch_examples.m
@@ -5,27 +5,31 @@ clc; clear; close all;
 load pointsRange.mat
 size(pointsRange)
 
-
-dimension = 2;
+max_dimension = 3;
 num_landmark_points = 50;
 num_divisions = 500;
+nu = 1;
 
-m_space = metric.impl.EuclideanMetricSpace(pointsRange)
-r_max = metric.utility.MetricUtility.estimateDiameter(m_space) / 2
-max_filtration_value = r_max / 5
+% Change so that max_filtration is selected using R.
+max_filtration_value = 1.2 / 8;
+%% m_space = metric.impl.EuclideanMetricSpace(pointsRange);
+%% r_max = metric.utility.MetricUtility.estimateDiameter(m_space) / 2
+%% max_filtration_value = r_max / 5
 
 % create a max-min landmark selector
 landmark_selector = api.Plex4.createMaxMinSelector(pointsRange, num_landmark_points);
 % create a Lazy-Witness Stream - note that this sets the number of
 % divisions to the default value of 20
-stream = api.Plex4.createLazyWitnessStream(landmark_selector, dimension + 1, max_filtration_value, num_divisions);
+stream = api.Plex4.createLazyWitnessStream(landmark_selector, max_dimension, max_filtration_value, nu, num_divisions);
+% ERROR - how do I specify both nu = 1 and num_divisions?
+%stream = api.Plex4.createLazyWitnessStream(landmark_selector, max_dimension, max_filtration_value, num_divisions);
 
 % print out the size of the stream - will be quite large since the complex
 % construction is very sensitive to the maximum filtration value
 size = stream.getSize()
 
 % get the default persistence algorithm
-persistence = api.Plex4.getDefaultSimplicialAlgorithm(dimension + 1);
+persistence = api.Plex4.getDefaultSimplicialAlgorithm(max_dimension);
 
 % compute the intervals and transform them to filtration values
 filtration_index_intervals = persistence.computeIntervals(stream);
--- src/matlab/for_distribution/tutorial_examples/lazy_witness_example.m
+++ src/matlab/for_distribution/tutorial_examples/lazy_witness_example.m
@@ -3,30 +3,33 @@
 
 clc; clear; close all;
 
-dimension = 2;
+max_dimension = 3;
 num_points = 1000;
 num_landmark_points = 100;
 max_filtration_value = 0.4;
+nu = 1;
+% error - int nu is interpreted as num_divisions!! Having trouble
+% using both nu and num_divisions as inputs. 
 
 % create the set of points
-point_cloud = examples.PointCloudExamples.getRandomSpherePoints(num_points, dimension);
+point_cloud = examples.PointCloudExamples.getRandomSpherePoints(num_points, max_dimension - 1);
 
 % create a randomized landmark selector
 landmark_selector = api.Plex4.createRandomSelector(point_cloud, num_landmark_points);
 % create a Lazy-Witness Stream - note that this sets the number of
 % divisions to the default value of 20
-stream = api.Plex4.createLazyWitnessStream(landmark_selector, dimension + 1, max_filtration_value);
+stream = api.Plex4.createLazyWitnessStream(landmark_selector, max_dimension, max_filtration_value, nu);
 
 % print out the size of the stream - will be quite large since the complex
 % construction is very sensitive to the maximum filtration value
 size = stream.getSize()
 
 % get the default persistence algorithm
-persistence = api.Plex4.getDefaultSimplicialAlgorithm(dimension + 1);
+persistence = api.Plex4.getDefaultSimplicialAlgorithm(max_dimension);
 
 % compute the intervals and transform them to filtration values
 filtration_index_intervals = persistence.computeIntervals(stream);
-filtration_value_intervals = stream.transform(filtration_index_intervals)
+filtration_value_intervals = stream.transform(filtration_index_intervals);
 
 % create the barcode plots
 api.Plex4.createBarcodePlot(filtration_value_intervals, 'lazySphere', max_filtration_value)
