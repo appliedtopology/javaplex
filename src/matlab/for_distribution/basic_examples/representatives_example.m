5de1cc6153ed44241722791c30ab34d04a1376bc
Andrew Tausz <andrew.tausz@gmail.com>
2011-06-20 16:53:11 +0000

    Added back in reprepresentative cycles example

---
+++ src/matlab/for_distribution/basic_examples/representatives_example.m
@@ -0,0 +1,20 @@
+% This script shows an Euler characteristic example - Section 7.1
+clc; clear; close all;
+
+% get a new ExplicitSimplexStream
+stream = api.Plex4.createExplicitSimplexStream();
+
+dimension = 9;
+
+% construct simplicial sphere
+stream.addElement(0:(dimension + 1));
+stream.ensureAllFaces();
+stream.removeElementIfPresent(0:(dimension + 1));
+stream.finalizeStream();
+
+% get the default persistence computation over Z/2Z
+persistence = api.Plex4.getModularSimplicialAlgorithm(dimension + 1, 2);
+
+% compute and print the intervals
+n_sphere_intervals = persistence.computeAnnotatedIntervals(stream)
+
