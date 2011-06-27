90db0431ba63e517d14ccd3d34abd401316d3c73
andrew.tausz@gmail.com <andrew.tausz@gmail.com@17b1e2b0-e439-11de-a347-f34a4f72eb7d>
2011-06-27 19:09:37 +0000

    Added endpoint extraction example. Removed unnecessary png files.

---
+++ src/matlab/for_distribution/basic_examples/endpoint_extraction_example.m
@@ -0,0 +1,31 @@
+% This script demonstrates how to extract endpoints
+
+clc; clear; close all;
+
+max_dimension = 2;
+max_filtration_value = 4;
+num_divisions = 100;
+
+% create the set of points
+point_cloud = examples.PointCloudExamples.getHouseExample();
+
+% create a Vietoris-Rips stream 
+stream = api.Plex4.createVietorisRipsStream(point_cloud, max_dimension + 1, max_filtration_value, num_divisions);
+
+% get the default persistence algorithm
+persistence = api.Plex4.getDefaultSimplicialAlgorithm(max_dimension);
+
+% this initially creates a set of intervals which contains the filtration
+% indices (which are integers).
+filtration_index_intervals = persistence.computeIntervals(stream);
+% this converts the filtration index intervals to the actual filtration
+% value intervals
+intervals = stream.transform(filtration_index_intervals)
+
+% The next line extracts the interval endpoints for thhe dimension 0
+% barcode. Note that it ignores infinite intervals
+endpoints_dim_0 = homology.barcodes.BarcodeUtility.getEndpoints(intervals, 0, true)
+
+% The next line extracts the interval endpoints for thhe dimension 1
+% barcode. Note that it ignores infinite intervals
+endpoints_dim_1 = homology.barcodes.BarcodeUtility.getEndpoints(intervals, 1, true)
