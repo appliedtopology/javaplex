e5bf101f8de287dba8186cd672e3d6725e41d1d9
andrew.tausz@gmail.com <andrew.tausz@gmail.com@17b1e2b0-e439-11de-a347-f34a4f72eb7d>
2011-07-09 23:44:39 +0000

    Updated vietoris-rips bootstrap code.

--- src/matlab/experimental/vietoris_rips_bootstrap/image_patch_single_example.m
+++ src/matlab/experimental/vietoris_rips_bootstrap/image_patch_single_example.m
@@ -2,17 +2,21 @@ clc; clear; close all;
 
 %%
 
-k_min = 25;
-k_max = 500;
+k_min = 500;
+k_max = 800;
 k_step = 25;
 
-T_min = 25;
-T_max = 300;
+T_min = 100;
+T_max = 400;
 T_step = 25;
 
-max_filtration_value = 0.04;
+max_filtration_value = 0.2;
 
-parfor k = k_min:k_step:k_max
+i_min = 0;
+i_max = (k_max - k_min) / k_step;
+
+parfor i = i_min:i_max
+    k = k_min + i * k_step;
     for T = T_min:T_step:T_max
         [barcodes, stream_size] = image_patch_vr(T, k, max_filtration_value, false)
     end
