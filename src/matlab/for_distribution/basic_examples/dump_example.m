807112095a918173c3f4a7d13016ac556442b657
Andrew Tausz <andrew.tausz@gmail.com>
2011-10-27 01:46:49 +0000

    Added dump example.

---
+++ src/matlab/for_distribution/basic_examples/dump_example.m
@@ -0,0 +1,25 @@
+% This script demonstrates the use of plex on a simple "house" example
+
+clc; clear; close all;
+
+max_dimension = 2;
+max_filtration_value = 4;
+num_divisions = 100;
+
+% create the set of points
+point_cloud = examples.PointCloudExamples.getHouseExample();
+
+% create a Vietoris-Rips stream 
+stream = api.Plex4.createVietorisRipsStream(point_cloud, max_dimension + 1, max_filtration_value, num_divisions);
+
+
+%% Dump Example:
+
+iterator = stream.iterator();
+
+while (iterator.hasNext())
+  % the next line will print the current simplex
+  simplex = iterator.next()
+  % here you can do whatever is needed with the simplex
+end
+
