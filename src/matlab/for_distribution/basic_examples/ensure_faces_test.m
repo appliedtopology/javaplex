92eec279cce420cb9380111843da6276623286c0
Andrew Tausz <andrew.tausz@gmail.com>
2012-01-17 18:01:36 +0000

    Added test for ensureAllFaces.

---
+++ src/matlab/for_distribution/basic_examples/ensure_faces_test.m
@@ -0,0 +1,21 @@
+clc; clear; close all;
+
+
+stream = api.Plex4.createExplicitSimplexStream();
+for i=1:4,
+    stream.addVertex(i,0);
+end
+stream.addElement([1,2,3,4], 2);
+stream.addElement([1,2,3], 1)
+stream.ensureAllFaces();
+stream.finalizeStream();
+
+iterator = stream.iterator();
+
+while (iterator.hasNext())
+  % the next line will print the current simplex
+  simplex = iterator.next();
+  % here you can do whatever is needed with the simplex
+  filtration_value = stream.getFiltrationIndex(simplex);
+  display(sprintf('%s: %d', char(simplex.toString()), filtration_value));
+end
