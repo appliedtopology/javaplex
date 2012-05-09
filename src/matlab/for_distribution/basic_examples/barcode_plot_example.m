8c5a8ad58b827209b322022c419acc540e4d7721
Andrew Tausz <andrew.tausz@gmail.com>
2012-05-09 00:01:09 +0000

    Added matlab eps plot example.

---
+++ src/matlab/for_distribution/basic_examples/barcode_plot_example.m
@@ -0,0 +1,27 @@
+% This script demonstrates the use of plex on a simple "house" example
+
+clc; clear; close all;
+
+max_dimension = 2;
+max_filtration_value = 4;
+num_divisions = 100;
+
+% create the set of points
+point_cloud = examples.PointCloudExamples.getHouseExample();
+
+% create a Vietoris-Rips stream 
+stream = api.Plex4.createVietorisRipsStream(point_cloud, max_dimension + 1, max_filtration_value, num_divisions);
+
+% get the default persistence algorithm
+persistence = api.Plex4.getDefaultSimplicialAlgorithm(max_dimension);
+
+% this initially creates a set of intervals which contains the filtration values
+filtration_value_intervals = persistence.computeIntervals(stream)
+
+% set plot options - plot in eps format
+options.filename = 'house-barcodes';
+options.file_format = 'eps';
+options.caption = 'House Example'
+
+% create the barcode plots
+plot_barcodes(filtration_value_intervals, options)
