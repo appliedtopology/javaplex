f211a8655ae2ed37fc7a803c92ecbf773650c86d
Andrew Tausz <andrew.tausz@gmail.com>
2012-08-10 18:53:03 +0000

    Various old changes again.

---
+++ src/matlab/experimental/hom/continuous/tetra_octa_example.m
@@ -0,0 +1,43 @@
+%% Continuous circle example - finds random corner point of feasible set
+
+clc; clear; close all;
+
+domain_size = 4;
+codomain_size = 5;
+
+domain_stream = examples.SimplexStreamExamples.getTetrahedron();
+codomain_stream = examples.SimplexStreamExamples.getOctahedron();
+
+domain_points = examples.PointCloudExamples.getTetrahedronVertices();
+codomain_points = examples.PointCloudExamples.getOctahedronVertices() + 1;
+
+% obtain the parameterization
+[cycle_sum, homotopies] = hom_parameterization(domain_stream, codomain_stream);
+
+% setup the LP - find a feasible point and the value of the max function in
+% the set C
+[f, A, b, Aeq, beq, lb, ub] = create_max_lp(cycle_sum, homotopies);
+[x, fval, exitflag, output, lambda] = linprog(f, A, b, Aeq, beq, lb, ub);
+fval
+
+map = compute_mapping(cycle_sum, homotopies, x(1:size(homotopies, 1)));
+map = (abs(map) > 1e-3) .* map;
+
+full(map)
+
+% find a random corner point of the set of optima
+[f, A, b, Aeq, beq, lb, ub] = create_max_lp(cycle_sum, homotopies, fval, randn(size(homotopies, 1), 1));
+[x, fval, exitflag, output, lambda] = linprog(f, A, b, Aeq, beq, lb, ub);
+fval
+
+map = compute_mapping(cycle_sum, homotopies, x(1:size(homotopies, 1)));
+map = (abs(map) > 1e-3) .* map;
+
+full(map)
+
+dlmwrite('matrix.txt', full(map));
+
+%%
+
+import edu.stanford.math.plex_viewer.*;
+Api.drawMapping(domain_stream, domain_points, codomain_stream, codomain_points, abs(map));
