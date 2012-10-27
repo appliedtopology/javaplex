9866db5f2d17833b3635ccbcab63e2216ba0946c
Andrew Tausz <andrew.tausz@gmail.com>
2012-10-27 21:33:34 +0000

    Added explicit simplex stream example.

---
+++ src/matlab/for_distribution/basic_examples/explicit_simplex_test2.m
@@ -0,0 +1,36 @@
+% This script demonstrates an example of the ExplicitSimplexStream class
+
+%% Manual construction
+
+clc; clear; close all;
+
+% get a new ExplicitSimplexStream
+stream = api.Plex4.createExplicitSimplexStream(10);
+
+% construct a triangle
+stream.addVertex(0, 0);
+stream.addVertex(1, 1);
+stream.addVertex(2, 2);
+stream.addElement([0, 1], 3.88);
+stream.addElement([0, 2], 4.12);
+stream.addElement([1, 2], 5);
+
+% print out the total number of simplices in the complex
+size = stream.getSize()
+
+
+
+iterator = stream.iterator();
+
+while (iterator.hasNext())
+  % the next line will print the current simplex
+  simplex = iterator.next()
+  % here you can do whatever is needed with the simplex
+  filtration_value = stream.getFiltrationValue(simplex)
+end
+
+
+
+
+
+
